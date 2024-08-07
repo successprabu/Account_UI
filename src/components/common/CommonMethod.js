fb39f148befe7b3d7de866495f9d70e554706ef2
antonyprabuv <96037775+antonyprabuv@users.noreply.github.com>
2024-08-07 18:42:55 +0530

    Token expiry changes

--- src/components/common/CommonMethod.js
+++ src/components/common/CommonMethod.js
@@ -19,6 +19,8 @@ const getToken = () => {
 const handleAuthenticationError = (error) => {
   if (error.response && error.response.status === 401) {
     // Handle unauthorized error, e.g., redirect to login
+    localStorage.removeItem('user'); // Remove user from localStorage
+    window.location.href = '/login'; // Redirect to login page
   }
   return Promise.reject(error);
 };
@@ -26,10 +28,9 @@ const handleAuthenticationError = (error) => {
 // Function to get the current date in UTC format
 export const dateUTC = (date = new Date()) => new Date(new Date(date).toUTCString()).toISOString();
 
-console.log(user,'localstorage')
 // Common payload fields for POST requests
 export const commonPayloadFields = {
-  createdBy: user ? JSON.parse(user).primary_phone:'APPLICATION',
+  createdBy: user ? JSON.parse(user).primary_phone : 'APPLICATION',
   createdDt: dateUTC(new Date()),
   updatedBy: 'APPLICATION',
   updatedDt: dateUTC(new Date()),
@@ -45,19 +46,19 @@ const post = (url, payload) => {
     url: BASE_URL + `${url}`,
     data: payload,
     headers: {
-      'Authorization':`Bearer ${getToken()}`
+      'Authorization': `Bearer ${getToken()}`
     }
   }).catch(handleAuthenticationError);
 };
 
 // GET method
-const get = (url,payload) => {
+const get = (url, payload) => {
   return axios({
     method: "get",
     url: BASE_URL + `${url}`,
     params: payload,
     headers: {
-      'Authorization':`Bearer ${getToken()}`      
+      'Authorization': `Bearer ${getToken()}`
     }
   }).catch(handleAuthenticationError);
 };
@@ -78,8 +79,8 @@ const postreq = (url, payload) => {
 
 // Export API service
 export const API_SERVICE = {
-  post: post,
-  get: get,
-  postreq:postreq,
-  formatDate:formatDate
+  post,
+  get,
+  postreq,
+  formatDate
 };
