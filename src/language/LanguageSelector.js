8cd13fded05d064d7f895a837f4e0d78bed67c29
antonyprabuv <96037775+antonyprabuv@users.noreply.github.com>
2024-08-13 01:33:55 -0500

    malayalm,Telungu, Hindi, Kanda

--- src/language/LanguageSelector.js
+++ src/language/LanguageSelector.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { useLanguage } from './LanguageContext';
-import { Dropdown } from 'react-bootstrap';
-import { FaLanguage } from 'react-icons/fa';
-import styled from 'styled-components';
+import React from "react";
+import { useLanguage } from "./LanguageContext";
+import { Dropdown } from "react-bootstrap";
+import { FaLanguage } from "react-icons/fa";
+import styled from "styled-components";
 
 // Styled components for custom dropdown styling
 const CustomDropdownToggle = styled(Dropdown.Toggle)`
-  background-color: ${({ bgColor }) => bgColor || '#0e2238'};
-  color: ${({ textColor }) => textColor || 'white'};
+  background-color: ${({ bgColor }) => bgColor || "#0e2238"};
+  color: ${({ textColor }) => textColor || "white"};
   border: none;
   &:hover {
-    background-color: ${({ hoverColor }) => hoverColor || '#0a1e2d'};
+    background-color: ${({ hoverColor }) => hoverColor || "#0a1e2d"};
   }
 `;
 
 const CustomDropdownMenu = styled(Dropdown.Menu)`
-  background-color: ${({ bgColor }) => bgColor || '#0e2238'};
+  background-color: ${({ bgColor }) => bgColor || "#0e2238"};
   .dropdown-item {
-    color: ${({ textColor }) => textColor || 'white'};
+    color: ${({ textColor }) => textColor || "white"};
     &:hover {
-      background-color: ${({ hoverColor }) => hoverColor || '#0a1e2d'};
+      background-color: ${({ hoverColor }) => hoverColor || "#0a1e2d"};
     }
   }
 `;
@@ -33,25 +33,37 @@ const LanguageSelector = ({ backgroundColor, textColor, hoverColor }) => {
 
   return (
     <Dropdown className="language-selector">
-      <CustomDropdownToggle 
-        id="dropdown-basic" 
-        bgColor={backgroundColor} 
-        textColor={textColor} 
+      <CustomDropdownToggle
+        id="dropdown-basic"
+        bgColor={backgroundColor}
+        textColor={textColor}
         hoverColor={hoverColor}
       >
         <FaLanguage className="language-icon" size={24} />
       </CustomDropdownToggle>
 
-      <CustomDropdownMenu 
-        bgColor={backgroundColor} 
-        textColor={textColor} 
+      <CustomDropdownMenu
+        bgColor={backgroundColor}
+        textColor={textColor}
         hoverColor={hoverColor}
       >
-        <Dropdown.Item onClick={() => handleLanguageChange('en')}>
+        <Dropdown.Item onClick={() => handleLanguageChange("en")}>
           English
         </Dropdown.Item>
-        <Dropdown.Item onClick={() => handleLanguageChange('ta')}>
+        <Dropdown.Item onClick={() => handleLanguageChange("ta")}>
           தமிழ்
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleLanguageChange("ml")}>
+          മലയാളം
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleLanguageChange("hi")}>
+          हिंदी
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleLanguageChange("te")}>
+          తెలుగు
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleLanguageChange("kn")}>
+          ಕನ್ನಡ
         </Dropdown.Item>
       </CustomDropdownMenu>
     </Dropdown>
--- src/language/i18n.js
+++ src/language/i18n.js
@@ -2,6 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './json/en.json';
 import ta from './json/ta.json';
+import ml from './json/ml.json';
+import te from './json/te.json';
+import kn from './json/kn.json';
+import hi from './json/hi.json';
 
 const resources = {
   en: {
@@ -9,6 +13,18 @@ const resources = {
   },
   ta: {
     translation: ta
+  },
+  ml: {
+    translation: ml
+  },
+  kn: {
+    translation: kn
+  },
+  te: {
+    translation: te
+  },
+  hi: {
+    translation: hi
   }
 };
 
